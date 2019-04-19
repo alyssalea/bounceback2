34b09b2eff4076aac0af54719b1b1e900f893f0a
alyssalea <alyssalea@icloud.com>
2019-04-19 10:16:19 -0400

    Add files via upload

---
+++ bounce back/sketch.js
@@ -0,0 +1,29 @@
+var x = 0
+var speed 
+
+
+function setup() {
+  createCanvas(600,600);
+  //background(255,0,255);
+}
+
+function draw() {
+   background(255,0,255)      
+   fill(0);
+  
+  if(x > 600){
+  speed =- 3;//or speed =- 3
+  }
+  
+   if(x < 1 ){
+  speed =+ 3;//or speed =+ 3
+  }
+  
+  ellipse(x, 300, 50)
+  
+  x = x + speed
+ 
+}
+
+
+  
